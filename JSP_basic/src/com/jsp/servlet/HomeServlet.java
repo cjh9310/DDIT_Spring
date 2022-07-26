25222442d7cda41a7fc586d22063debf45e33fc4
DESKTOP-SS23CQV\pc-22 <d>
2022-07-26 09:15:26 +0900

    day

---
+++ JSP_basic/src/com/jsp/servlet/HomeServlet.java
@@ -0,0 +1,42 @@
+package com.jsp.servlet;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HomeServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url="/home.jsp";
+		
+		String today  = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+		
+		// System.out.println(today);
+		
+		request.setAttribute("today", today);
+		
+		
+		request.getRequestDispatcher(url).forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+

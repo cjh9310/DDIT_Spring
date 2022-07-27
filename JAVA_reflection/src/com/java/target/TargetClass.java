10dffe229b0513cda5f7580c03868f3f3e84e908
DESKTOP-SS23CQV\pc-22 <d>
2022-07-27 19:01:44 +0900

    reflection

---
+++ JAVA_reflection/src/com/java/target/TargetClass.java
@@ -0,0 +1,18 @@
+package com.java.target;
+
+public class TargetClass {
+	
+	
+	public void a() {
+		System.out.println("a method");
+	}
+	public void a(String str) {
+		System.out.println(str);
+	}
+	public void b() {
+		System.out.println("b method");
+	}
+	public void c() {
+		System.out.println("c method");
+	}
+}

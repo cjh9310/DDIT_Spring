1d60f8019079dfc4fcf26d184c39d24c3556d590
cjh9310 <mine931045@gmail.com>
2022-08-24 18:29:28 +0900

    JSP_board

---
+++ JSP_command_bootstrap/src/com/jsp/action/board/BoardDetailAction.java
@@ -0,0 +1,37 @@
+package com.jsp.action.board;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jsp.action.Action;
+import com.jsp.dto.BoardVO;
+import com.jsp.service.BoardService;
+
+public class BoardDetailAction implements Action{
+
+	private BoardService boardService;
+	public void setBoardService(BoardService boardService) {
+		this.boardService = boardService;
+	}
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String url ="/board/detail";
+		
+		int bno = Integer.parseInt(request.getParameter("bno"));
+		
+		String from = request.getParameter("from");
+		
+		BoardVO board = null;
+		
+		if(from != null && from.equals("list")) {
+			board = boardService.getBoard(bno);
+			url = "redirect/board/detail.do?bno=" + bno;
+		}else {
+			board = boardService.getBoardForModify(bno);
+		}
+		
+		request.setAttribute("board",board);
+		return url;
+	}
+
+}
---
+++ JSP_command_bootstrap/src/com/jsp/action/board/BoardRegistAction.java
@@ -0,0 +1,29 @@
+package com.jsp.action.board;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jsp.action.Action;
+import com.jsp.controller.HttpRequestParameterAdapter;
+import com.jsp.dto.BoardVO;
+import com.jsp.service.BoardService;
+
+public class BoardRegistAction implements Action{
+	
+	private BoardService boardService;
+	public void setBoardService(BoardService boardService) {
+		this.boardService = boardService;
+	}
+	
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String url = "/board/regist_success";
+		
+		BoardVO board = HttpRequestParameterAdapter.execute(request, BoardVO.class);
+		
+		boardService.regist(board);
+		
+		return url;
+	}
+
+}
